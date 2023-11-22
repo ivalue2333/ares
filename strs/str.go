93981bb3c213ad96cfab20114c8d32dee7650abc
zhuyuanbing <zhuyuanbing.percy@bytedance.com>
2023-11-22 16:54:05 +0800

    feat: add float
    Change-Id: Ia5b007bc7d75c319a1996a3221799e72eb609533

--- strs/str.go
+++ strs/str.go
@@ -1,16 +1,36 @@
 package strs
 
-import "strconv"
+import (
+	"strconv"
+
+	"github.com/ivalue2333/ares/internal/constraints"
+)
 
 // Str like python str.
-func Str[T int | int8 | int16 | int32 | int64](v T) string {
-	return strconv.FormatInt(int64(v), 10)
+func Str[T constraints.Integer](v T) string {
+	if v < 0 {
+		return strconv.FormatInt(int64(v), 10)
+	}
+	return strconv.FormatUint(uint64(v), 10)
 }
 
-func Strs[T int | int8 | int16 | int32 | int64](vv []T) []string {
+func StrF[T constraints.Float](v T) string {
+	return strconv.FormatFloat(float64(v), 'g', -1, 64)
+}
+
+func Strs[T constraints.Integer](vv []T) []string {
 	res := make([]string, 0, len(vv))
 	for _, v := range vv {
 		s := Str(v)
+		res = append(res, s)
+	}
+	return res
+}
+
+func StrFs[T constraints.Float](vv []T) []string {
+	res := make([]string, 0, len(vv))
+	for _, v := range vv {
+		s := StrF(v)
 		res = append(res, s)
 	}
 	return res
